9340e42465970daeb3d3feaede5c7e9a4bde6817
Casa Taloyum <casatwy@msn.com>
2018-04-25 11:41:16 +0800

    modification

--- CTHandyCategories/CTHandyCategories/NSString/CTDate/NSString+CTDate.m
+++ CTHandyCategories/CTHandyCategories/NSString/CTDate/NSString+CTDate.m
@@ -13,10 +13,11 @@
 - (NSDate *)ct_dateFromRFC3339
 {
     NSDateFormatter *rfc3339DateFormatter = [[NSDateFormatter alloc] init];
-    rfc3339DateFormatter.dateFormat = @"yyyy-MM-dd'T'HH:mm:ss.SSSSSSSSSxxx";
+    rfc3339DateFormatter.dateFormat = @"yyyy-MM-dd'T'HH:mm:ss.SSSSSS";
 //    rfc3339DateFormatter.timeZone = [NSTimeZone systemTimeZone];
 //    rfc3339DateFormatter.locale = [NSLocale currentLocale];
-    return [rfc3339DateFormatter dateFromString:self];
+    NSDate *result = [rfc3339DateFormatter dateFromString:self];
+    return result;
 }
 
 @end
